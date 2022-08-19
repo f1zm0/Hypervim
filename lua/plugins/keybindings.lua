8e0a6e918a2db93d7ac5700ba9c45b1a10104047
fizm0 <79873476+fizm0@users.noreply.github.com>
2022-08-19 02:11:18 +0200

    feat: added keybindings for both core and plugins

--- lua/plugins/keybindings.lua
+++ lua/plugins/keybindings.lua
@@ -28,13 +28,19 @@ end
 -- Trouble
 local trouble_status_ok, _ = pcall(require, 'trouble')
 if trouble_status_ok then
-  keymap('n', '<leader>t', '<cmd>TroubleToggle<cr>', opts)
+  keymap('n', '<leader>tt', '<cmd>TroubleToggle<cr>', opts)
   keymap('n', '<leader>tw', '<cmd>Trouble workspace_diagnostics<cr>', opts)
   keymap('n', '<leader>td', '<cmd>Trouble document_diagnostics<cr>', opts)
   keymap('n', '<leader>tq', '<cmd>Trouble quickfix<cr>', opts)
 end
 
--- Sesions
+-- Todos
+local todo_status_ok, _ = pcall(require, 'todo-comments')
+if todo_status_ok then
+  keymap('n', '<leader>ft', '<cmd>TodoTrouble<cr>', opts)
+end
+
+-- Sessions
 local autosession_status_ok, _ = pcall(require, 'auto-session')
 if autosession_status_ok then
   keymap('n', '<c-s>', '<cmd>SaveSession<cr>', opts)
@@ -43,10 +49,4 @@ end
 local sessionlens_status_ok, _ = pcall(require, 'session-lens')
 if sessionlens_status_ok then
   keymap('n', '<leader>fs', '<cmd>SearchSession<cr>', opts)
-end
-
--- Todos
-local todo_status_ok, _ = pcall(require, 'todo-comments')
-if todo_status_ok then
-  keymap('n', '<leader>ft', '<cmd>TodoTrouble<cr>', opts)
 end
