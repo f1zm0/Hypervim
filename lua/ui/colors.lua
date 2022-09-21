fa751a755c7919bf3932b338bbf4bf9bc1551cf8
fizm0 <79873476+fizm0@users.noreply.github.com>
2022-09-22 00:44:42 +0200

    fix: updated tokyonight config structure and custom colors

--- lua/ui/colors.lua
+++ lua/ui/colors.lua
@@ -1,20 +1,41 @@
-vim.opt.termguicolors = true
-vim.opt.background = 'dark'
+local status_ok, tokyonight = pcall(require, 'tokyonight')
+if not status_ok then
+  return
+end
 
--- theme style
-vim.g.material_style = 'deep ocean'
-
--- -- theme setup
-require('material').setup({
-  contrast = {
-    sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-    floating_windows = false, -- Enable contrast for floating windows
-    line_numbers = false, -- Enable contrast background for line numbers
-    sign_column = true, -- Enable contrast background for the sign column
-    cursor_line = false, -- Enable darker background for the cursor line
-    non_current_windows = true, -- Enable darker background for non-current windows
-    popup_menu = false, -- Enable lighter background for the popup menu
+tokyonight.setup({
+  style = 'moon',
+  transparent = false,
+  terminal_colors = true,
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    sidebars = 'dark', -- style for sidebars, see below
+    floats = 'dark', -- style for floating windows
   },
+  sidebars = { 'qf', 'help' },
+  day_brightness = 0.3,
+  hide_inactive_statusline = false,
+  dim_inactive = false,
+  lualine_bold = false,
+
+  on_colors = function(colors)
+    colors.green1 = '#15b3e9'
+    colors.orange = '#96c5fa'
+    colors.blue = '#72a4fd'
+    colors.purple = '#c58efb'
+    colors.fg = '#cadafe'
+    -- colors.bg = '#1a2129'
+    -- colors.bg_float = '#202933'
+    colors.bg_sidebar = '#242435'
+    colors.border_highlight = '#2b2f43'
+  end,
+
+  on_highlights = function(highlights, c)
+    highlights.NvimTreeFolderIcon = { bg = c.none, fg = c.orange }
+  end,
 })
 
-vim.cmd([[colorscheme material]])
+vim.cmd([[colorscheme tokyonight]])
