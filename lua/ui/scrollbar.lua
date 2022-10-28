cd26ad1be32f38221ed0e4d56a9dbb9083f8a0bb
fizm0 <79873476+fizm0@users.noreply.github.com>
2022-10-28 22:16:35 +0200

    feat(scrollbar): added custom options for scrollbar

--- lua/ui/scrollbar.lua
+++ lua/ui/scrollbar.lua
@@ -3,4 +3,26 @@ if not status_ok then
   return
 end
 
-scrollbar.setup()
+local colors = require('tokyonight.colors').setup()
+
+scrollbar.setup({
+  show_in_active_only = true,
+  excluded_filetypes = {
+    'prompt',
+    'TelescopePrompt',
+    'noice',
+    'Trouble',
+    'NvimTree',
+  },
+  handle = {
+    color = colors.bg_highlight,
+  },
+  marks = {
+    Search = { color = colors.orange },
+    Error = { color = colors.error },
+    Warn = { color = colors.warning },
+    Info = { color = colors.info },
+    Hint = { color = colors.hint },
+    Misc = { color = colors.purple },
+  },
+})
