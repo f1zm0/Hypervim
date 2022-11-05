5e4a1e5b4a3f97d9de82ef75566cddcfd057ea3e
fizm0 <79873476+fizm0@users.noreply.github.com>
2022-11-05 20:05:37 +0100

    feat(treesitter): disabled highlight for big files and en

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,7 +25,7 @@ require('nvim-treesitter.configs').setup({
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   auto_install = true,
@@ -34,4 +34,13 @@ require('nvim-treesitter.configs').setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+
+  -- disable highlighting for huge files
+  disable = function(_, buf)
+    local max_filesize = 100 * 1024 -- 100 KB
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if ok and stats and stats.size > max_filesize then
+      return true
+    end
+  end,
 })
