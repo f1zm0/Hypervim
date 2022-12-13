51e8340e7514b83b83728a00464401ea04945c90
fizm0 <79873476+fizm0@users.noreply.github.com>
2022-12-13 21:22:55 +0100

    style: better styling for telescope panels

--- lua/hvim/plugins/telescope.lua
+++ lua/hvim/plugins/telescope.lua
@@ -13,13 +13,30 @@ telescope.setup({
       'vendor',
       'node_modules',
     },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = 'smart_case',
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = 'smart_case',
+      },
+    },
+    prompt_prefix = '   ',
+    selection_caret = '  ',
+    entry_prefix = '  ',
+    sorting_strategy = 'ascending',
+    layout_strategy = 'flex',
+    layout_config = {
+      horizontal = {
+        prompt_position = 'top',
+        preview_width = 0.55,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.8,
+      preview_cutoff = 120,
     },
   },
 })
