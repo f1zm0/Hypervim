cf8e683c6f54d9da90801981b4f63dd423bf1a0a
fizm0 <79873476+fizm0@users.noreply.github.com>
2022-12-30 23:53:52 +0100

    style: change scrollbar color using colorscheme ones

--- lua/hvim/plugins/scrollbar.lua
+++ lua/hvim/plugins/scrollbar.lua
@@ -3,7 +3,7 @@ if not status_ok then
   return
 end
 
-local colors = require('tokyonight.colors').setup()
+local colors = require('tokyonight.colors').neo
 
 scrollbar.setup({
   show_in_active_only = true,
@@ -15,14 +15,14 @@ scrollbar.setup({
     'NvimTree',
   },
   handle = {
-    color = '#454545',
+    color = colors.fg_gutter,
   },
   marks = {
     Search = { color = colors.orange },
-    Error = { color = colors.error },
-    Warn = { color = colors.warning },
-    Info = { color = colors.info },
-    Hint = { color = colors.hint },
+    Error = { color = colors.red },
+    Warn = { color = colors.yellow },
+    Info = { color = colors.blue },
+    Hint = { color = colors.magenta },
     Misc = { color = colors.purple },
   },
 })
