8ceee58844a179b36cef15564e1fea6c558982bd
f1zm0 <root@fzm.ooo>
2023-01-24 09:21:05 +0100

    feat: remove tab key conflicts between cmp and copilot

--- lua/hvim/plugins/cmp/init.lua
+++ lua/hvim/plugins/cmp/init.lua
@@ -19,6 +19,11 @@ end
 -- kind icons
 local kind_icons = require('hvim.ui.icons').completion
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
@@ -64,6 +69,8 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
     ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
@@ -72,7 +79,31 @@ cmp.setup({
       select = true,
       behavior = cmp.ConfirmBehavior.Replace,
     }),
-    ['Right'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if require('copilot.suggestion').is_visible() then
+        require('copilot.suggestion').accept()
+      elseif cmp.visible() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      'i',
+      's',
+    }),
+    ['<S-Tab>'] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
   }),
   experimental = {
     ghost_text = {
