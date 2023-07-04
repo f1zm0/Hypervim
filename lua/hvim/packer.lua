4e47dbf502a451567ab9a9a351cb57d8f0e26f17
f1zm0 <root@fzm.ooo>
2023-07-04 22:18:30 +0200

    fix: use fidget legacy branch to avoid breaking changes

--- lua/hvim/packer.lua
+++ lua/hvim/packer.lua
@@ -65,7 +65,7 @@ packer.startup(function(use)
   use('ray-x/lsp_signature.nvim')
   use('onsails/lspkind.nvim')
   use('folke/trouble.nvim')
-  use('j-hui/fidget.nvim')
+  use({ 'j-hui/fidget.nvim', tag = 'legacy' }) -- use legacy until rewrite
 
   -- Formatting
   use('jose-elias-alvarez/null-ls.nvim')
