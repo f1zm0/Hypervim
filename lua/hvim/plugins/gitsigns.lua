d3012fc5ae5d4bc73a05d2a64d138a74189a7a72
f1zm0 <root@fzm.ooo>
2023-07-04 22:19:56 +0200

    fix: remove keymap option for gitsigns

--- lua/hvim/plugins/gitsigns.lua
+++ lua/hvim/plugins/gitsigns.lua
@@ -10,7 +10,6 @@ return {
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  keymaps = {},
   watch_gitdir = {
     interval = 1000,
     follow_files = true,
