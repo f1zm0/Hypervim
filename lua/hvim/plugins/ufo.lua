d4a68653e054195796fb5f79220467b340ddefdb
f1zm0 <root@fzm.ooo>
2025-04-26 12:51:54 +0000

    feat: add ufo for better code folding

---
+++ lua/hvim/plugins/ufo.lua
@@ -0,0 +1,17 @@
+return {
+   "kevinhwang91/nvim-ufo",
+   enabled = true,
+   dependencies = {
+      { "kevinhwang91/promise-async" },
+   },
+   config = function()
+      -- Fold options
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "0" -- fold signs in col 0 -> don't show
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      require("ufo").setup()
+   end,
+}
