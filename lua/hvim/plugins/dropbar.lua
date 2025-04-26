654b7ff3bcd3e73ca7c1cdcae034f4da65ba7972
f1zm0 <root@fzm.ooo>
2025-04-26 12:49:58 +0000

    refactor: replace archived barbecue with dropbar

---
+++ lua/hvim/plugins/dropbar.lua
@@ -0,0 +1,12 @@
+return {
+   'Bekaboo/dropbar.nvim',
+   enabled = true,
+   -- no popup dropdown, just breadcumbs
+   dependencies = {},
+   -- config = function()
+   --    local dropbar_api = require('dropbar.api')
+   --    vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+   --    vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+   --    vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+   -- end
+}
