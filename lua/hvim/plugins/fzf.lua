5528b43c1edfaaf590fc2f02b32772820933d6b7
f1zm0 <root@fzm.ooo>
2025-05-01 19:46:05 +0000

    chore: update fzflua opts for rg and fd

--- lua/hvim/plugins/fzf.lua
+++ lua/hvim/plugins/fzf.lua
@@ -2,18 +2,26 @@ return {
    'ibhagwan/fzf-lua',
    keys = {
       { '<leader>ff', '<cmd>FzfLua files no_header_i=true<cr>', desc = 'Find files' },
-      { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'Find (grep) in files' },
-      { '<leader>fb', '<cmd>FzfLua buffers<cr>', desc = 'Find buffers' },
-      { '<leader>fh', '<cmd>FzfLua help_tags<cr>', desc = 'Find tags' },
-      { '<leader>fr', '<cmd>FzfLua oldfiles<cr>', desc = 'Find recent files' },
-      { '<leader>x', '<cmd>FzfLua diagnostics_document<cr>', desc = 'Show document diagnostics' },
-      { '<leader>X', '<cmd>FzfLua diagnostics_workspace<cr>', desc = 'Show workspace diagnostics' },
+      { '<leader>fg', '<cmd>FzfLua live_grep<cr>',              desc = 'Find (grep) in files' },
+      { '<leader>fb', '<cmd>FzfLua buffers<cr>',                desc = 'Find buffers' },
+      { '<leader>fh', '<cmd>FzfLua help_tags<cr>',              desc = 'Find tags' },
+      { '<leader>fr', '<cmd>FzfLua oldfiles<cr>',               desc = 'Find recent files' },
+      { '<leader>x',  '<cmd>FzfLua diagnostics_document<cr>',   desc = 'Show document diagnostics' },
+      { '<leader>X',  '<cmd>FzfLua diagnostics_workspace<cr>',  desc = 'Show workspace diagnostics' },
    },
    dependencies = { 'nvim-tree/nvim-web-devicons' },
    opts = {
+      grep = {
+         rg_opts = string.format(
+            [[--color=never --no-heading --line-number --column --smart-case --hidden --follow --ignore-file=%s]],
+            vim.fn.expand("$HOME/.config/ignore/vim-ignore")
+         ),
+      },
       files = {
-         rg_opts = "--files --hidden --column --line-number --no-heading --color=never --smart-case --follow -g '!.git/' --ignore-file=.gitignore",
-         fd_opts = "--color=never --type f --hidden --follow --exclude .git --ignore-file .gitignore",
+         fd_opts = string.format(
+            [[--color=never --type f --hidden --follow --ignore-file=%s]],
+            vim.fn.expand("$HOME/.config/ignore/vim-ignore")
+         ),
       },
       oldfiles = {
          cwd_only = true,
@@ -21,7 +29,7 @@ return {
       },
       previewers = {
          builtin = {
-            syntax_limit_b = 1024 * 100, -- disable preview if >=100KB, just to be safe
+            syntax_limit_b = 1024 * 100, -- disable preview if file size >=100KB to avoid performance issues
          },
       },
    },
