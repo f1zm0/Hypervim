3fda5024e68462342bdcea0857b464f4d3470c1e
f1zm0 <root@fzm.ooo>
2025-05-01 19:23:27 +0000

    feat: add nvim-lint

---
+++ lua/hvim/plugins/nvimlint.lua
@@ -0,0 +1,24 @@
+return {
+   "mfussenegger/nvim-lint",
+   lazy = true,
+   event = { "BufReadPre", "BufNewFile" },
+   config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+         javascript = { "eslint_d" },
+         typescript = { "eslint_d" },
+         vue = { "eslint_d" },
+         go = { "golangcilint" },
+      }
+      -- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      -- group = lint_augroup,
+      -- callback = function()
+      -- lint.try_lint()
+      -- end,
+      -- })
+      vim.keymap.set("n", "<leader>li", function()
+         lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+   end,
+}
