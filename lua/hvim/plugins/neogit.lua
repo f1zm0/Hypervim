ece81e406b47771bbeb9c70ca51e2a971d439ffa
f1zm0 <root@fzm.ooo>
2025-10-11 20:31:10 +0000

    feat: add neogit plugin config

---
+++ lua/hvim/plugins/neogit.lua
@@ -0,0 +1,10 @@
+return {
+   "NeogitOrg/neogit",
+   keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+   },
+   dependencies = {
+      "nvim-lua/plenary.nvim",
+      "ibhagwan/fzf-lua",
+   },
+}
