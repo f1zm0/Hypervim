8b22356bc11eae0007514f24d6c73274f7cc1528
f1zm0 <root@fzm.ooo>
2025-10-18 19:55:42 +0000

    feat: add ruff linter for python

--- lua/hvim/plugins/nvimlint.lua
+++ lua/hvim/plugins/nvimlint.lua
@@ -9,6 +9,7 @@ return {
          typescript = { "eslint_d" },
          vue = { "eslint_d" },
          go = { "golangcilint" },
+         python = { "ruff" }
       }
       -- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
       -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
