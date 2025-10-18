04b257cd5e7958ce77d50dc2e6bc042cd7183d82
f1zm0 <root@fzm.ooo>
2025-10-18 19:55:53 +0000

    refactor: update nvimlint augroup cmd

--- lua/hvim/plugins/nvimlint.lua
+++ lua/hvim/plugins/nvimlint.lua
@@ -11,15 +11,11 @@ return {
          go = { "golangcilint" },
          python = { "ruff" }
       }
-      -- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-      -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      -- group = lint_augroup,
-      -- callback = function()
-      -- lint.try_lint()
-      -- end,
-      -- })
-      vim.keymap.set("n", "<leader>li", function()
-         lint.try_lint()
-      end, { desc = "Trigger linting for current file" })
+      -- auto lint on save
+      vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+         callback = function()
+            lint.try_lint()
+         end,
+      })
    end,
 }
